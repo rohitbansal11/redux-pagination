1c14e85b08f0a1d317c5400b3e13fe3ccc8d9fc9
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2021-10-15 12:03:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import User from './main/file/User'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <User />
+     
+    </div>
+  );
+}
+
+export default App;
