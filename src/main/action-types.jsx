0fc3e8d55fd6f45e0ff84c6727059856b897e976
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2021-10-15 12:04:45 +0530

    Add files via upload

---
+++ src/main/action-types.jsx
@@ -0,0 +1,5 @@
+export const GET_USERS = 'GET_USERS';
+export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const Get_Current = 'Get_Current';
+export const USERS_ERROR = 'USERS_ERROR';
+export const EDIT_EMPLOYEE = 'EDIT_EMPLOYEE';
